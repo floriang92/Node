66aa223007e7e41512553c6d4c365ece6de54dcc
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2022-04-06 15:16:43 +0200

    Backend /folderDetail

--- Backend/app.js
+++ Backend/app.js
@@ -1,79 +1,82 @@
-const express = require('express');
-const path = require('path');
-const fileController = require('./controllers/fileController')
-const cors = require('cors');
-
+const express = require("express");
+const path = require("path");
+const fileController = require("./controllers/fileController");
+const folderController = require("./controllers/folderController");
+const cors = require("cors");
 
 ////////////////////////////////////////////////////////////////////////////// APP SETUP //////////////////////////////////////////////////////////////
 
-
 const app = express();
-app.use(cors({
-    origin: "*"
-  }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
-
 ////////////////////////////////////////////////////////////////////////////// APP ROUTES FILES //////////////////////////////////////////////////////////////
 
-
-app.get('/files', async function (req, res, next) {
-    let allFiles = await fileController.GetAllFiles()
-        .then((result) => {
-            return (result);
-        })
-        .catch((error) => {
-            return (error)
-        })
-
-    res.json(allFiles)
-})
-
-app.get('/file/:id', async function (req, res, next) {
-    let oneFile = await fileController.GetOneFile(req.params.id)
-        .then((result) => {
-            return (result);
-        })
-        .catch((error) => {
-            return (error)
-        })
-    res.json(oneFile)
-})
-
-app.post('/file', async function (req, res, next) {
-    let addedFile = await fileController.AddFile(req.body)
-        .then((result) => {
-            return result;
-        })
-        .catch((error) => {
-            return error
-        })
-    res.json(addedFile)
-})
-
-app.put('/file/:id', async function (req, res, next) {
-    let updatedFile = await fileController.UpdateFile(req.params.id, req.body)
+app.get("/folderDetail", async function (req, res, next) {
+  let oneFolder = await folderController
+    .GetOneFolder()
     .then((result) => {
-        res.status(200).send("Update OK")
+      return result;
     })
     .catch((error) => {
-        res.status(400).send(error)
-    })
-  })
+      return error;
+    });
 
-  app.delete('/file/:id', async function (req, res, next) {
-    let deletedFile = await fileController.DeleteFile(req.params.id)
-    .then((result) => {
-        res.status(200).send("Deletion OK")
-    })
-    .catch((error) => {
-        res.status(400).send(error)
-    })
-  })
-
-app.listen(port, () => {
-    console.log('Server app listening on port ' + port);
+  res.json(oneFolder);
 });
 
+app.get("/file/:id", async function (req, res, next) {
+  let oneFile = await fileController
+    .GetOneFile(req.params.id)
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      return error;
+    });
+  res.json(oneFile);
+});
+
+app.post("/file", async function (req, res, next) {
+  let addedFile = await fileController
+    .AddFile(req.body)
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      return error;
+    });
+  res.json(addedFile);
+});
+
+app.put("/file/:id", async function (req, res, next) {
+  let updatedFile = await fileController
+    .UpdateFile(req.params.id, req.body)
+    .then((result) => {
+      res.status(200).send("Update OK");
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+app.delete("/file/:id", async function (req, res, next) {
+  let deletedFile = await fileController
+    .DeleteFile(req.params.id)
+    .then((result) => {
+      res.status(200).send("Deletion OK");
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log("Server app listening on port " + port);
+});
---
+++ Backend/controllers/folderController.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+
+function getOneFolder() {
+  return fs.readdirSync(__dirname, (err, files) => {
+    if (err) {
+      return err;
+    } else {
+      return files;
+    }
+  });
+}
+
+function getAllFolders() {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .model("Folder")
+      .find({})
+      .exec(function (err, folders) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(folders);
+        }
+      });
+  });
+}
+
+function createFolder(data) {
+  return new Promise((resolve, reject) => {
+    mongoose.model("Folder").create(data, function (err, folder) {
+      if (err) {
+        console.log(err);
+        reject("Can't add folder in DataBase, add Immatriculation field");
+      } else {
+        resolve(folder);
+      }
+    });
+  });
+}
+
+function updateFolder(id, data) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .model("Folder")
+      .findOneAndUpdate(
+        { _id: id },
+        data,
+        { returnOriginal: false },
+        function (err, folder) {
+          if (err) {
+            console.log(err);
+            reject("Can't update folder");
+          } else {
+            resolve(folder);
+          }
+        }
+      );
+  });
+}
+
+function deleteFolder(id) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .model("Folder")
+      .findOneAndDelete({ _id: id }, function (err, folder) {
+        if (err) {
+          console.log(err);
+          reject("Can't delete folder");
+        } else {
+          resolve("Folder delete OK");
+        }
+      });
+  });
+}
+
+module.exports = {
+  GetOneFolder: async () => {
+      console.log(getOneFolder())
+    return getOneFolder();
+  },
+
+  GetAllFolders: async () => {
+    let allFolders = await getAllFolders();
+    return allFolders;
+  },
+
+  AddFolder: async (data) => {
+    let addedFolder = await createFolder(data);
+    return addedFolder;
+  },
+
+  UpdateFolder: async (id, data) => {
+    let updatedFolder = await updateFolder(id, data);
+    return updatedFolder;
+  },
+
+  DeleteFolder: async (id) => {
+    let deletedFolder = await deleteFolder(id);
+    return deletedFolder;
+  },
+};
