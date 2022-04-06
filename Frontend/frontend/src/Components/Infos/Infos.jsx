defd934df40bed8fee0de945e13d087c9827a46b
WilliamH <58976208+Willix-IT@users.noreply.github.com>
2022-04-06 13:24:56 +0200

    Detailed display

--- Frontend/frontend/src/Components/Infos/Infos.jsx
+++ Frontend/frontend/src/Components/Infos/Infos.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./Infos.css";
+import { Divider } from 'antd';
 export default function Infos() {
   return (
     <div style={{display:"flex", flexDirection:"column", width: "20vw"}} >
       <div>
-          <h3>About</h3>
-          <p>Line content</p><br/>
-          <p>Line content</p><br/>
-          <p>Line content</p><br/>
-          <p>Line content</p><br/>
+          <h4>About</h4>
+          <p>@ Readme</p>
+          <p>@ 0 stars</p>
+          <p>@ 2 watching</p>
+          <p>@ 3 forks</p>
       </div>
+      <hr color="#8B949E" style={{width:"100%"}}/>
       <div>
-          <p>Infos</p>
+        <h4>Releases</h4>
+          <p>V.1.0.0-alpha.01</p>
       </div>
+      <hr color="#8B949E" style={{width:"100%"}}/>
       <div>
-          <p>Infos</p>
+        <h4>Packages</h4>
+          <p>No packages published</p>
       </div>
+      <hr color="#8B949E" style={{width:"100%"}}/>
       <div>
-          <p>Infos</p>
+        <h4>Contributors</h4>
+          <p>Raphael Da Fonseca</p>
+          <p>William Herbin</p>
+          <p>Florian Gillet</p>
       </div>
     </div>
   );
