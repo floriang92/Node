ac94258c6461abffcdbc0146a81b6ecb610119c1
Raphael <raphael@arcplex.fr>
2022-04-06 15:56:25 +0200

    update content by request

--- Frontend/frontend/src/Environnement.js
+++ Frontend/frontend/src/Environnement.js
@@ -1,6 +1,6 @@
 
 const Name = "GitLike"
-
+const baseUrl = "http://localhost:3000"
 const Color1 = "#161B22" // header
 const Color2 = "#0D1117" //background
 const Color3 = "#58A6FF" // bouton
@@ -8,4 +8,4 @@ const Color4 = "#13233A" // card
 const Color5 = "#2EA043" // bouton vert
 const Color6 = "#8B949E" // text light
 
-export {Name, Color1, Color2, Color3, Color4, Color5, Color6 }
+export {Name, Color1, Color2, Color3, Color4, Color5, Color6, baseUrl }
